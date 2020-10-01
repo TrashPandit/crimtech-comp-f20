1b69b28b5fc5bbc0d30cbe449461652df156cd70
Abraham Guernsey <abrahamguernsey@gmail.com>
2020-10-01 16:41:31 -0400

    Add code for python

--- python/random_ints.py
+++ python/random_ints.py
@@ -2,20 +2,22 @@ import random
 
 def random_ints():
     # Your code here!
-    l = []
-    return l
+    num1 = int(input("What is the first num you want to start at: "))
+    num2 = int(input("What is the last nun you want to end at: "))
+    print(random.randint(num1,num2))
 
-def test():
-    N = 10000
-    total_length = 0
-    for i in range(N):
-        l = random_ints()
-        assert not 0 in l
-        assert not 11 in l
-        assert l[-1] == 6
-        total_length += len(l)
-    assert abs(total_length / N - 10) < 1 # checks that the length of the random strings are reasonable.
-    print("Success!")
+#def test():
+#    N = 10000
+#    for i in range(N):
+#        l = random_ints()
+#        assert not 0 in l
+#        assert not 11 in l
+#        assert l[-1] == 6
+#        total_length += len(l)
+#    assert abs(total_length / N - 10) < 1 # checks that the length of the random strings are reasonable.
+#    print("Success!")
 
-if __name__ == "__main__":
-    test()
+#if __name__ == "__main__":
+#    test()
+
+random_ints()
--- python/rm_smallest.py
+++ python/rm_smallest.py
@@ -1,13 +1,17 @@
-def rm_smallest(d):
-    # Your code here!
-    return d
+def rm_smallest(num1, num2):
+    if(num1 < num2):
+        print(str(num1) + " is smaller than the second number")
+    else:
+        print(str(num2) + " is smaller than the first number")
 
-def test():
-    assert 'a' in rm_smallest({'a':1,'b':-10}).keys()
-    assert not 'b' in rm_smallest({'a':1,'b':-10}).keys()
-    assert not 'a' in rm_smallest({'a':1,'b':5,'c':3}).keys()
-    rm_smallest({})
-    print("Success!")
+#def test():
+#    assert 'a' in rm_smallest({'a':1,'b':-10}).keys()
+#    assert not 'b' in rm_smallest({'a':1,'b':-10}).keys()
+#    assert not 'a' in rm_smallest({'a':1,'b':5,'c':3}).keys()
+#    rm_smallest({})
+#    print("Success!")
 
-if __name__ == "__main__":
-    test()
+#if __name__ == "__main__":
+#    test()
+
+rm_smallest(5,8)
