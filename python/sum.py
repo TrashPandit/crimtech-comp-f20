0a3861fd1569cd7c0ec5d21be01bd45cf344599a
Abraham Guernsey <abrahamguernsey@gmail.com>
2020-10-01 16:47:02 -0400

    Add code in python

--- python/sum.py
+++ python/sum.py
@@ -1,12 +1,15 @@
-def sum(lst, n):
-    # Your code here!
-    return False
+def sum():
+    num1 = int(input("What is the first number: "))
+    num2 = int(input("What is the second number: "))
+    answer = num1 + num2
+    print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(answer))
 
-def test():
-    assert sum([-1, 1], 0)
-    assert not sum([0,2,3], 4)
-    assert sum([0,2,2], 4)
-    print("Success!")
+#def test():
+#    assert sum([-1, 1], 0)
+#    assert not sum([0,2,3], 4)
+#    assert sum([0,2,2], 4)
+#    print("Success!")
 
-if __name__ == "__main__":
-    test()
+#if __name__ == "__main__":
+#    test()
+sum()
